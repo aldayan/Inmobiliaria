8e7e14b803220c02aa60390d7dac666f11ce890f
Aldayan <20187144@itla.edu.do>
2023-05-08 14:09:09 -0400

    estilos finales

---
+++ assets/js/menu.js
@@ -0,0 +1,14 @@
+var menu= document.getElementById('inicio');
+
+
+windows.addEvenListener('scroll', ()=>{
+
+var scroll = windows.scrollY;
+
+if(scroll>10){
+menu,style.backgroundColor='rgb(141, 84, 9)';
+}else{
+menu,style.backgroundColor='transparent';
+}
+
+});
--- index.php
+++ index.php
@@ -157,6 +157,7 @@ include("./dba/db.php");
 
    
     <script type="module" src="./assets/js/index-producto.js"></script>
+    <script type="module" src="./assets/js/menu.js"></script>
     <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
 </body>
 
